5f0e15d5ff2dc699c2473d587fd467a89c297af9
tak <re795h@gmail.com>
2025-06-14 16:01:24 +0900

    tmp debug query

--- src/db/index.ts
+++ src/db/index.ts
@@ -115,6 +115,7 @@ export class Database {
 		// build parameters
 		const parameters: any[] = [`[${query.join(",")}]`, k];
 		if (tableName === "document_chunk") parameters.push(filter ?? "{}");
+		logger.debug(`sql: ${sql}`);
 		return await this.appDataSource.query(sql, parameters);
 	}
 
